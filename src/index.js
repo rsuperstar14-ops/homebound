3b7ec91f2fe9f46ba4b7f4efc0ab2b3ae39cd659
rsuperstar14-ops <r.superstar14@gmail.com>
2026-05-19 19:01:06 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./styles.css";
 
-const root = createRoot(document.getElementById("root"));
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
